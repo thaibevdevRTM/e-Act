2e39768da83f770b48d94ec782edacf877d7d1dd
parnupongk <parnupongk@gmail.com>
2019-04-03 13:42:38 +0700

    update actform report detail

---
+++ eActForm/eActForm/Models/RepDetailModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eActForm.Models
+{
+    public class RepDetailModel
+    {
+        public class actFormRepDetails
+        {
+            public List<actFormRepDetail> repDetailLists { get; set; }
+        }
+        public class actFormRepDetail : Activity_Model.actForm
+        {
+            public string cusNameTH { get; set; }
+            public string productId { get; set; }
+            public string productName { get; set; }
+            public string size { get; set; }
+            public string typeTheme { get; set; }
+            public string normalSale { get; set; }
+            public string promotionSale { get; set; }
+            public string total { get; set; }
+        }
+    }
+}
