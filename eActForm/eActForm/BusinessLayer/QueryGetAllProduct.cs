7f6aa72159bb30436508491469b93018e17fccc1
parnupongk <parnupongk@gmail.com>
2019-04-17 17:39:00 +0700

    fixed bug filter product

--- eActForm/eActForm/BusinessLayer/QueryGetAllProduct.cs
+++ eActForm/eActForm/BusinessLayer/QueryGetAllProduct.cs
@@ -37,12 +37,15 @@ namespace eActForm.BusinessLayer
                 throw new Exception("getProductSmellByGroupId >>" + ex.Message);
             }
         }
-        public static List<TB_Act_Product_Model.Product_Model> getProductBySmellId(string smellId)
+        public static List<TB_Act_Product_Model.Product_Model> getProductBySmellIdAndBrandId(string smellId, string brandId)
         {
             try
             {
-                DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getProductBySmellId"
-                    , new SqlParameter("@smellId", smellId));
+                DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getProductBySmellIdAndBrandId"
+                    , new SqlParameter[] {
+                        new SqlParameter("@smellId", smellId)
+                        ,new SqlParameter("@brandId",brandId)
+                    });
                 var lists = (from DataRow d in ds.Tables[0].Rows
                              select new TB_Act_Product_Model.Product_Model()
                              {
@@ -67,11 +70,45 @@ namespace eActForm.BusinessLayer
                 return new List<TB_Act_Product_Model.Product_Model>();
             }
         }
-        public static List<TB_Act_Product_Model.Product_Model> getAllProduct()
+        public static List<TB_Act_Product_Model.Product_Model> getProductBySmellId(string smellId,string productGroupId)
         {
             try
             {
-                DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getAllProduct");
+                DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getProductBySmellId"
+                    ,new SqlParameter[] {
+                        new SqlParameter("@smellId", smellId)
+                        ,new SqlParameter("@productGroupId",productGroupId)
+                    });
+                var lists = (from DataRow d in ds.Tables[0].Rows
+                             select new TB_Act_Product_Model.Product_Model()
+                             {
+                                 id = d["id"].ToString(),
+                                 productCode = d["productCode"].ToString(),
+                                 productName = d["productName"].ToString(),
+                                 brandId = d["brandId"].ToString(),
+                                 size = d["size"].ToString() == "" ? "0" : d["size"].ToString(),
+                                 unit = d["unit"].ToString() == "" ? 0 : Convert.ToInt32(d["unit"].ToString()),
+                                 litre = d["litre"].ToString() == "" ? 0 : Convert.ToInt32(d["litre"].ToString()),
+                                 delFlag = (bool)d["delFlag"],
+                                 createdDate = DateTime.Parse(d["createdDate"].ToString()),
+                                 createdByUserId = d["createdByUserId"].ToString(),
+                                 updatedDate = DateTime.Parse(d["updatedDate"].ToString()),
+                                 updatedByUserId = d["updatedByUserId"].ToString(),
+                             });
+                return lists.ToList();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.WriteError("getProductBySmellId => " + ex.Message);
+                return new List<TB_Act_Product_Model.Product_Model>();
+            }
+        }
+        public static List<TB_Act_Product_Model.Product_Model> getAllProduct(string productGroupId)
+        {
+            try
+            {
+                DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getAllProduct"
+                    ,new SqlParameter[] { new SqlParameter("@productGroupId", productGroupId) });
                 var lists = (from DataRow d in ds.Tables[0].Rows
                              select new TB_Act_Product_Model.Product_Model()
                              {
