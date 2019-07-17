1ccb9f9cef05990dbc283b08858bdfe565fec44e
tanapong <tanapong.w@thaibev.com>
2019-07-17 16:46:55 +0700

    dev add product Master

--- eActForm/eActForm/BusinessLayer/QueryHandler/QueryGetAllPrice.cs
+++ eActForm/eActForm/BusinessLayer/QueryHandler/QueryGetAllPrice.cs
@@ -3,6 +3,7 @@ using Microsoft.ApplicationBlocks.Data;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using WebLibrary;
@@ -12,13 +13,13 @@ namespace eActForm.BusinessLayer
     public class QueryGetAllPrice
     {
 
-        public static List<TB_Act_ProductPrice_Model.ProductPrice_Model> getAllPrice()
+        public static List<TB_Act_ProductPrice_Model.ProductPrice> getAllPrice()
         {
             try
             {
                 DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getAllProductPrice");
                 var lists = (from DataRow d in ds.Tables[0].Rows
-                             select new TB_Act_ProductPrice_Model.ProductPrice_Model()
+                             select new TB_Act_ProductPrice_Model.ProductPrice()
                              {
                                  id = d["id"].ToString(),
                                  productCode = d["productCode"].ToString(),
@@ -26,18 +27,49 @@ namespace eActForm.BusinessLayer
                                  price = decimal.Parse(d["price"].ToString()),
                                  startDate = DateTime.Parse(d["startDate"].ToString()),
                                  endDate = DateTime.Parse(d["endDate"].ToString()),
-                                 delFlag = d["delFlag"].ToString(),
+                                 delFlag = bool.Parse(d["delFlag"].ToString()),
                                  createdDate = DateTime.Parse(d["createdDate"].ToString()),
-                                 createdBy = d["createdByUserId"].ToString(),
+                                 createdByUserId = d["createdByUserId"].ToString(),
                                  updatedDate = DateTime.Parse(d["updatedDate"].ToString()),
-                                 updatedBy = d["updatedByUserId"].ToString(),
+                                 updatedByUserId = d["updatedByUserId"].ToString(),
                              });
                 return lists.ToList();
             }
             catch(Exception ex)
             {
                 ExceptionManager.WriteError("getActivityById => " + ex.Message);
-                return new List<TB_Act_ProductPrice_Model.ProductPrice_Model>();
+                return new List<TB_Act_ProductPrice_Model.ProductPrice>();
+            }
+        }
+
+
+        public static List<TB_Act_ProductPrice_Model.ProductPrice> getPriceByProductCode(string p_productCode)
+        {
+            try
+            {
+                DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "ups_getProductPriceByProductCode"
+                      , new SqlParameter("@productcode", p_productCode));
+                var lists = (from DataRow d in ds.Tables[0].Rows
+                             select new TB_Act_ProductPrice_Model.ProductPrice()
+                             {
+                                 productCode = d["productId"].ToString(),
+                                 productName = d["productName"].ToString(),
+                                 customerName = d["cusNameEN"].ToString(),
+                                 customerId = d["customerId"].ToString(),
+                                 normalCost = d["normalCost"] == null ? 0 : decimal.Parse(AppCode.checkNullorEmpty(d["normalCost"].ToString())),
+                                 wholeSalesPrice = d["wholeSalesPrice"] == null ? 0 : decimal.Parse(AppCode.checkNullorEmpty(d["wholeSalesPrice"].ToString())),
+                                 discount1 = d["discount1"] == null ? 0 :decimal.Parse(AppCode.checkNullorEmpty(d["discount1"].ToString())),
+                                 discount2 = d["discount2"] == null ? 0 :decimal.Parse(AppCode.checkNullorEmpty(d["discount2"].ToString())),
+                                 discount3 = d["discount3"] == null ? 0 : decimal.Parse(AppCode.checkNullorEmpty(d["discount3"].ToString())),
+                                 saleNormal = d["normalCost"] == null ? 0 : decimal.Parse(AppCode.checkNullorEmpty(d["normalCost"].ToString())),
+                                 updatedDate = DateTime.Parse(d["updatedDate"].ToString()),
+                             });
+                return lists.ToList();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.WriteError("getPriceByProductCode => " + ex.Message);
+                return new List<TB_Act_ProductPrice_Model.ProductPrice>();
             }
         }
     }
