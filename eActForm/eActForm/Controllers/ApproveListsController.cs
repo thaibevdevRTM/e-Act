5121f995d2379c221e3f71df86686a88c5d5047a
parnupongk <parnupongk@gmail.com>
2019-09-18 13:57:33 +0700

    add status all in approve list page

--- eActForm/eActForm/Controllers/ApproveListsController.cs
+++ eActForm/eActForm/Controllers/ApproveListsController.cs
@@ -14,6 +14,12 @@ namespace eActForm.Controllers
         public ActionResult Index()
         {
             SearchActivityModels models = SearchAppCode.getMasterDataForSearch();
+            models.approveStatusList.Add(new ApproveModel.approveStatus()
+            {
+                id = "7",
+                nameTH = "ทั้งหมด",
+                nameEN = "All",
+            });
             return View(models);
         }
 
@@ -25,7 +31,7 @@ namespace eActForm.Controllers
                 model = new Activity_Model.actForms();
                 model.actLists = ApproveListAppCode.getApproveListsByEmpId(UtilsAppCode.Session.User.empId);
                 TempData["ApproveFormLists"] = model.actLists;
-                model.actLists = ApproveListAppCode.getFilterFormByStatusId(model.actLists,(int)AppCode.ApproveStatus.รออนุมัติ);
+                model.actLists = ApproveListAppCode.getFilterFormByStatusId(model.actLists, (int)AppCode.ApproveStatus.รออนุมัติ);
             }
             else
             {
@@ -33,7 +39,7 @@ namespace eActForm.Controllers
             }
             return PartialView(model);
         }
-        
+
 
         public ActionResult searchActForm()
         {
@@ -41,13 +47,27 @@ namespace eActForm.Controllers
             Activity_Model.actForms model = new Activity_Model.actForms();
             model.actLists = (List<Activity_Model.actForm>)TempData["ApproveFormLists"];
 
-            if (Request.Form["txtActivityNo"] != "")
-            {
-                model.actLists = model.actLists.Where(r => r.activityNo == Request.Form["txtActivityNo"]).ToList();
-            }else if (Request.Form["ddlStatus"] != "")
+            if (Request.Form["ddlStatus"] != "" && Request.Form["ddlStatus"] != "7")
             {
                 model.actLists = ApproveListAppCode.getFilterFormByStatusId(model.actLists, int.Parse(Request.Form["ddlStatus"]));
             }
+            if (Request.Form["txtActivityNo"] != "")
+            {
+                model.actLists = model.actLists.Where(r => r.activityNo == Request.Form["txtActivityNo"]).ToList();
+            }
+            if (Request.Form["ddlCustomer"] != "")
+            {
+                model.actLists = model.actLists.Where(r => r.customerId == Request.Form["ddlCustomer"]).ToList();
+            }
+            if (Request.Form["ddlTheme"] != "")
+            {
+                model.actLists = model.actLists.Where(r => r.theme == Request.Form["ddlTheme"]).ToList();
+            }
+            if (Request.Form["ddlProductType"] != "")
+            {
+                model.actLists = model.actLists.Where(r => r.productTypeId == Request.Form["ddlProductType"]).ToList();
+            }
+
             TempData["ApproveSearchResult"] = model.actLists;
             return RedirectToAction("ListView");
         }
