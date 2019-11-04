59c3871e996b5ed8d80c5f5a3efa83806f8e3d8a
tanapong <tanapong.w@thaibev.com>
2019-11-04 15:26:18 +0700

    remove check user AD

--- eActForm/eActForm/BusinessLayer/Appcodes/UserAppCode.cs
+++ eActForm/eActForm/BusinessLayer/Appcodes/UserAppCode.cs
@@ -70,58 +70,5 @@ namespace eActForm.BusinessLayer
                 throw new Exception("setRoleUser>>" + ex.Message);
             }
         }
-
-
-        public static bool checkUserDBAuthen()
-        {
-
-            int result = 0;
-            bool insertResult = false;
-            try
-            {
-                 result = SqlHelper.ExecuteNonQuery(AppCode.StrCon, CommandType.StoredProcedure, "usp_checkUserDBAuthenInsert"
-                    , new SqlParameter[] {new SqlParameter("@empId",UtilsAppCode.Session.User.empId)
-                    ,new SqlParameter("@empStatus",UtilsAppCode.Session.User.empStatus)
-                    ,new SqlParameter("@empPrefix",UtilsAppCode.Session.User.empPrefix)
-                    ,new SqlParameter("@empGender",UtilsAppCode.Session.User.empGender)
-                    ,new SqlParameter("@empFNameEN",UtilsAppCode.Session.User.empFNameEN)
-                    ,new SqlParameter("@empLNameEN",UtilsAppCode.Session.User.empLNameEN)
-                    ,new SqlParameter("@empFNameTH",UtilsAppCode.Session.User.empFNameTH)
-                    ,new SqlParameter("@empLNameTH",UtilsAppCode.Session.User.empLNameTH)
-                    ,new SqlParameter("@empDateofBirth",UtilsAppCode.Session.User.empDateofBirth)
-                    ,new SqlParameter("@empMaritalStatus",UtilsAppCode.Session.User.empMaritalStatus)
-                    ,new SqlParameter("@empNationality",UtilsAppCode.Session.User.empNationality)
-                    ,new SqlParameter("@empPosition",UtilsAppCode.Session.User.empPosition)
-                    ,new SqlParameter("@empPositionTitleEN",UtilsAppCode.Session.User.empPositionTitleEN)
-                    ,new SqlParameter("@empPositionTitleTH",UtilsAppCode.Session.User.empPositionTitleTH)
-                    ,new SqlParameter("@empLocalPositionEN",UtilsAppCode.Session.User.empLocalPositionEN)
-                    ,new SqlParameter("@empLocalPositionTH",UtilsAppCode.Session.User.empLocalPositionTH)
-                    ,new SqlParameter("@empExternalPositionEN",UtilsAppCode.Session.User.empExternalPositionEN)
-                    ,new SqlParameter("@empExternalPositionTH",UtilsAppCode.Session.User.empExternalPositionTH)
-                    ,new SqlParameter("@empPositionLevel",UtilsAppCode.Session.User.empPositionLevel)
-                    ,new SqlParameter("@empPositionRange",UtilsAppCode.Session.User.empPositionRange)
-                    ,new SqlParameter("@empLevel",UtilsAppCode.Session.User.empLevel)
-                    ,new SqlParameter("@empClass",UtilsAppCode.Session.User.empClass)
-                    ,new SqlParameter("@empType",UtilsAppCode.Session.User.empType)
-                    ,new SqlParameter("@empCategory",UtilsAppCode.Session.User.empCategory)
-                    ,new SqlParameter("@empCompanyGroup",UtilsAppCode.Session.User.empCompanyGroup)
-                    ,new SqlParameter("@empCompanyId",UtilsAppCode.Session.User.empCompanyId)
-                    ,new SqlParameter("@empDivisionEN",UtilsAppCode.Session.User.empDivisionEN)
-                    ,new SqlParameter("@empDivisionTH",UtilsAppCode.Session.User.empDivisionTH)
-                    ,new SqlParameter("@empDepartmentEN",UtilsAppCode.Session.User.empDepartmentEN)
-                    ,new SqlParameter("@empDepartmentTH",UtilsAppCode.Session.User.empDepartmentTH)
-                    ,new SqlParameter("@empManagerId",UtilsAppCode.Session.User.empManagerId)
-                    ,new SqlParameter("@empEmail",UtilsAppCode.Session.User.empEmail)
-                    });
-
-                if (result > 0) insertResult = true;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("checkUserDBAuthen >> " + ex.Message);
-            }
-
-            return insertResult;
-        }
     }
 }
