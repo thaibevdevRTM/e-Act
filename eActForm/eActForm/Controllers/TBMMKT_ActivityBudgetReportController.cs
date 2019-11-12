2fb152edc1c2df8977d7286f6972ccee2e3e959c
peerapop.int <peerapop.i@thaibev.com>
2019-11-12 16:45:36 +0700

    bak20191112

---
+++ eActForm/eActForm/Controllers/TBMMKT_ActivityBudgetReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eActForm.Controllers
+{
+    public class TBMMKT_ActivityBudgetReportController : Controller
+    {
+        // GET: TBMMKT_ActivityBudgetReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
