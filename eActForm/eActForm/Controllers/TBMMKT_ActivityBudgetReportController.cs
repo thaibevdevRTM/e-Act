e11dbbc23a414ec1a1ee01531a81283f86f2deed
peerapop.int <peerapop.int@gmail.com>
2019-11-13 12:04:11 +0700

    bak20191113_1204

--- eActForm/eActForm/Controllers/TBMMKT_ActivityBudgetReportController.cs
+++ eActForm/eActForm/Controllers/TBMMKT_ActivityBudgetReportController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using eActForm.Models;
+using iTextSharp.text;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using WebLibrary;
 
 namespace eActForm.Controllers
 {
@@ -12,6 +17,28 @@ namespace eActForm.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult printDoc(string gridHtml)
+        {
+            List<Attachment> file = new List<Attachment>();
+            try
+            {
+                var rootPathInsert = string.Format(ConfigurationManager.AppSettings["rooPdftURL"], "");
+                gridHtml = gridHtml.Replace("<br>", "<br/>");
+                file = AppCode.genPdfFile(gridHtml, new Document(PageSize.A4, 25, 25, 10, 10), "");
+
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.WriteError(ex.Message);
+            }
+            return File(file[0].ContentStream, "application/pdf", "reportPDF.pdf");
+
         }
     }
 }
