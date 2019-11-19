ea85a2cc3624002986d1d6fefe80d0717893204a
peerapop.int <peerapop.int@gmail.com>
2019-11-19 17:01:55 +0700

    back 20191119

---
+++ eActForm/eActForm/Controllers/TBMMKT_ActivityBudgetInputController.cs
@@ -0,0 +1,109 @@
+﻿using eActForm.BusinessLayer;
+using eActForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLibrary;
+
+namespace eActForm.Controllers
+{
+    public class TBMMKT_ActivityBudgetInputController : Controller
+    {
+        // GET: TBMMKT_ActivityBudgetInput
+        public ActionResult Index(string activityId, string mode, string typeForm)
+        {
+            Activity_TBMMKT_Model activity_TBMMKT_Model = new Activity_TBMMKT_Model();
+            try
+            {
+
+                //activity_TBMMKT_Model.activityFormModel = new ActivityForm();
+                //activity_TBMMKT_Model.productSmellLists = new List<TB_Act_Product_Model.ProductSmellModel>();
+
+                //if (typeForm == Activity_Model.activityType.OMT.ToString())
+                //{
+                //    activity_TBMMKT_Model.customerslist = QueryGetAllCustomers.getCustomersOMT();
+                //}
+                //else
+                //{
+                //    activity_TBMMKT_Model.customerslist = QueryGetAllCustomers.getCustomersMT();
+                //}
+
+                //activity_TBMMKT_Model.productcatelist = QuerygetAllProductCate.getAllProductCate().ToList();
+                //activity_TBMMKT_Model.activityGroupList = QueryGetAllActivityGroup.getAllActivityGroup()
+                //    .GroupBy(item => item.activitySales)
+                //    .Select(grp => new TB_Act_ActivityGroup_Model { id = grp.First().id, activitySales = grp.First().activitySales }).ToList();
+                //if (UtilsAppCode.Session.User.regionId != "")
+                //{
+                //    activity_TBMMKT_Model.regionGroupList = QueryGetAllRegion.getAllRegion().Where(x => x.id == UtilsAppCode.Session.User.regionId).ToList();
+                //    activity_TBMMKT_Model.activityFormModel.regionId = UtilsAppCode.Session.User.regionId;
+                //}
+                //else
+                //{
+                //    activity_TBMMKT_Model.regionGroupList = QueryGetAllRegion.getAllRegion();
+                //}
+
+                if (!string.IsNullOrEmpty(activityId))
+                {
+
+                    activity_TBMMKT_Model.activityFormModel = QueryGetActivityById.getActivityById(activityId).FirstOrDefault();
+                    activity_TBMMKT_Model.activityFormModel.mode = mode;
+                    //activity_TBMMKT_Model.productcostdetaillist1 = QueryGetCostDetailById.getcostDetailById(activityId);
+                    //activity_TBMMKT_Model.activitydetaillist = QueryGetActivityDetailById.getActivityDetailById(activityId);
+                    //activity_TBMMKT_Model.productSmellLists = QueryGetAllProduct.getProductSmellByGroupId(activity_TBMMKT_Model.activityFormModel.productGroupId);
+                    //activity_TBMMKT_Model.productBrandList = QueryGetAllBrand.GetAllBrand().Where(x => x.productGroupId == activity_TBMMKT_Model.activityFormModel.productGroupId).ToList();
+                    //activity_TBMMKT_Model.productGroupList = QueryGetAllProductGroup.getAllProductGroup().Where(x => x.cateId == activity_TBMMKT_Model.activityFormModel.productCateId).ToList();
+                    TempData["actForm" + activityId] = activity_TBMMKT_Model;
+                }
+                else
+                {
+                    string actId = Guid.NewGuid().ToString();
+                    activity_TBMMKT_Model.activityFormModel.id = actId;
+                    //activity_TBMMKT_Model.activityFormModel.mode = mode;
+                    //activity_TBMMKT_Model.activityFormModel.statusId = 1;
+                    TempData["actForm" + actId] = activity_TBMMKT_Model;
+                }
+
+                activity_TBMMKT_Model.activityFormModel.typeForm = typeForm;
+                TempData.Keep();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.WriteError("TBMMKT_ActivityBudgetInputController => " + ex.Message);
+            }
+            return View(activity_TBMMKT_Model);
+        }
+
+        public JsonResult insertDataActivity(ActivityFormTBMMKT activityFormTBMMKT)
+        {
+            var result = new AjaxResult();
+            //try
+            //{
+            //    string statusId = "";
+            //    Activity_Model activityModel = TempData["actForm" + activityFormModel.id] == null ? new Activity_Model() : (Activity_Model)TempData["actForm" + activityFormModel.id];
+            //    activityModel.activityFormModel = activityFormModel;
+            //    statusId = ActivityFormCommandHandler.getStatusActivity(activityFormModel.id);
+            //    if (statusId == "1" || statusId == "5" || statusId == "")
+            //    {
+            //        int countSuccess = ActivityFormCommandHandler.insertAllActivity(activityModel, activityFormModel.id);
+            //    }
+            //    else
+            //    {
+            //        result.MessageCode = "001";
+            //    }
+
+            //    TempData.Keep();
+            //    result.Success = true;
+            //}
+            //catch (Exception ex)
+            //{
+            //    result.Success = false;
+            //    result.Message = ex.Message;
+            //    ExceptionManager.WriteError("insertDataActivity => " + ex.Message);
+            //}
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ eActForm/eActForm/Models/Activity_TBMMKT_Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using static eActForm.Models.TB_Act_Customers_Model;
+using static eActForm.Models.TB_Act_Product_Cate_Model;
+using static eActForm.Models.TB_Act_Product_Model;
+
+namespace eActForm.Models
+{
+    public class Activity_TBMMKT_Model : Activity_Model
+    {
+        public TB_Act_ActivityForm_DetailOther TB_Act_ActivityForm_DetailOther { get; set; }
+    }
+
+    public class ActivityFormTBMMKT : ActivityForm
+    {
+
+    }
+
+    public class TB_Act_ActivityForm_DetailOther
+    {
+        public string Id { get; set; }
+        public string activityNo { get; set; }
+        public string activityProduct { get; set; }
+        public string activityTel { get; set; }
+        public string EO { get; set; }
+        public Boolean delFlag { get; set; }
+        public DateTime? createdDate { get; set; }
+        public string createdByUserId { get; set; }
+        public DateTime? updatedDate { get; set; }
+        public string updatedByUserId { get; set; }
+    }
+
+}
