518335ab3f960bfd084d798c428a6999d4abd50b
tanapong <tanapong.w@thaibev.com>
2019-11-20 15:59:38 +0700

    commit edit code with company

--- eActForm/eActForm/Models/Activity_Model.cs
+++ eActForm/eActForm/Models/Activity_Model.cs
@@ -53,7 +53,8 @@ namespace eActForm.Models
         public enum activityType
         {
             OMT,
-            MT
+            MT,
+            TBM
         }
 
 
@@ -175,6 +176,7 @@ namespace eActForm.Models
         public Boolean chkAddIO { get; set; }
         public string actIO { get; set; }
         public string actClaim { get; set; }
+        public string companyId { get; set; }
     }
 
     public class CostThemeDetailOfGroupByPrice : ActBaseModel
