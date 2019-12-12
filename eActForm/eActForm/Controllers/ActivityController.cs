7d1d09fbd94e4f12e0f91ae5fecd31f2e5572f55
tanapong <tanapong.w@thaibev.com>
2019-12-12 13:15:57 +0700

    fix show img

--- eActForm/eActForm/Controllers/ActivityController.cs
+++ eActForm/eActForm/Controllers/ActivityController.cs
@@ -242,24 +242,8 @@ namespace eActForm.Controllers
                     string UploadDirectory = Server.MapPath(string.Format(System.Configuration.ConfigurationManager.AppSettings["rootUploadfiles"].ToString(), _fileName));
                     resultFilePath = UploadDirectory;
                     BinaryReader b = new BinaryReader(httpPostedFile.InputStream);
-                    if (BaseAppCodes.ValidateExtension(extension))
-                    {
-                        WebImage img = new WebImage(httpPostedFile.InputStream);
-                        if (img.Height > 700 || img.Width > 600)
-                        {
-                            img.Resize(550, 700);
-                            img.Save(UploadDirectory);
-                        }
-                        else
-                        {
-                            img.Save(UploadDirectory);
-                        }
-                    }
-                    else
-                    {
-                        httpPostedFile.SaveAs(UploadDirectory);
-                    }
-
+                    httpPostedFile.SaveAs(UploadDirectory);
+                    
                     imageFormModel.activityId = actId;
                     imageFormModel._image = b.ReadBytes(0); 
                     imageFormModel.imageType = "UploadFile";
