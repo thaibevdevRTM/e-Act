9039a66b3e630b906037f3b25843d1223779beb4
tanapong <tanapong.w@thaibev.com>
2020-01-06 15:16:25 +0700

    add contact eact

--- eActForm/eActForm/Controllers/HomeController.cs
+++ eActForm/eActForm/Controllers/HomeController.cs
@@ -179,5 +179,12 @@ namespace eActForm.Controllers
         {
             return View();
         }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
