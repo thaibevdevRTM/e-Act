f0c1b1fb1dfefe074de15fcd855370571731b4e8
tanapong <tanapong.w@thaibev.com>
2020-01-06 15:24:07 +0700

    commit

--- eActForm/eActForm/Models/Activity_Model.cs
+++ eActForm/eActForm/Models/Activity_Model.cs
@@ -98,8 +98,8 @@ namespace eActForm.Models
             public decimal? perTotal { get; set; }
             public string createByUserName { get; set; }
             public string regionId { get; set; }
-            public string brandId { get; set; }
-            
+            public string brandId { get; set; }            
+
         }
 
     }
@@ -178,6 +178,7 @@ namespace eActForm.Models
         public Boolean chkAddIO { get; set; }
         public string actIO { get; set; }
         public string actClaim { get; set; }
+        public string idTypeForm { get; set; }
     }
 
     public class CostThemeDetailOfGroupByPrice : ActBaseModel
---
+++ eActForm/eActForm/Models/MainFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eActForm.Models
+{
+    public class MainFormModel : Activity_TBMMKT_Model
+    {
+       
+    }
+
+}
