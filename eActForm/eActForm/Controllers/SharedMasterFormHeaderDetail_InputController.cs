fcf8c6361b77dcd2286a2ee84bafad3cf4c4f80d
peerapop.int <peerapop.int@gmail.com>
2020-01-27 16:50:50 +0700

    bak20200127_1650

--- eActForm/eActForm/Controllers/SharedMasterFormHeaderDetail_InputController.cs
+++ eActForm/eActForm/Controllers/SharedMasterFormHeaderDetail_InputController.cs
@@ -22,5 +22,9 @@ namespace eActForm.Controllers
         {
             return PartialView(activity_TBMMKT_Model);
         }
+        public ActionResult headerDetails_Pos_Premium(Activity_TBMMKT_Model activity_TBMMKT_Model)
+        {
+            return PartialView(activity_TBMMKT_Model);
+        }
     }
 }
