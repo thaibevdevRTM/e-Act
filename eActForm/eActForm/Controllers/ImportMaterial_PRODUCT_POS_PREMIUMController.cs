311511c100c29aac73e2cd391e91a7822395d5e6
peerapop.int <peerapop.int@gmail.com>
2020-02-12 16:45:46 +0700

    bak20200212_1645

--- eActForm/eActForm/Controllers/ImportMaterial_PRODUCT_POS_PREMIUMController.cs
+++ eActForm/eActForm/Controllers/ImportMaterial_PRODUCT_POS_PREMIUMController.cs
@@ -31,9 +31,73 @@ namespace eActForm.Controllers
                 dt = ExcelAppCode.ReadExcel(path, "dataimport", "A:H");
                 var countDataHaveRows = dt.Rows.Count;
 
+                if (countDataHaveRows > 0)
+                {
+                    //====add column to use insert db=========
+                    dt.Columns.Add("tB_Act_master_list_choice_id", typeof(String));
+                    dt.Columns.Add("tB_Act_master_list_choice_id_InOutStock", typeof(String));
+                    //==END==add column to use insert db======
+
+                    //=====validate=======
+                    bool validateValue = true;
+                    string txtError = "";
+                    for (int i = 0; i < countDataHaveRows; i++)
+                    {
+                        int rowInExcelAlert = (i + 2);
+                        if (dt.Rows[i]["Material"].ToString() == "")
+                        {
+                            validateValue = false;
+                            txtError += "กรุณาระบุ Material บรรทัดที่ " + rowInExcelAlert + "<br />";
+                        }
+                        if (dt.Rows[i]["Sub Group"].ToString() == "")
+                        {
+                            validateValue = false;
+                            txtError += "กรุณาระบุ Sub Group บรรทัดที่ " + rowInExcelAlert + "<br />";
+                        }
+                        if (dt.Rows[i]["Status Mat"].ToString() == "")
+                        {
+                            validateValue = false;
+                            txtError += "กรุณาระบุ Status Mat บรรทัดที่ " + rowInExcelAlert + "<br />";
+                        }
+
+                        string idFormPOS_Premium = "24BA9F57-586A-4A8E-B54C-00C23C41BFC5";
+                        var keyValue_subGroupIdByRow = QueryGet_TB_Act_master_list_choice.get_TB_Act_master_list_choice(idFormPOS_Premium, "product_pos_premium").Where(x => x.name == dt.Rows[i]["Sub Group"].ToString().ToUpper()).FirstOrDefault();
+                        if (keyValue_subGroupIdByRow != null)
+                        {
+                            dt.Rows[i]["tB_Act_master_list_choice_id"] = keyValue_subGroupIdByRow.id;
+                        }
+                        else
+                        {
+                            validateValue = false;
+                            txtError += "รูปแบบข้อมูลที่กรอกไม่ถูกต้อง Sub Group บรรทัดที่ " + rowInExcelAlert + "<br />";
+                        }
+                        var keyValue_statusMatIdByRow = QueryGet_TB_Act_master_list_choice.get_TB_Act_master_list_choice(idFormPOS_Premium, "in_or_out_stock").Where(x => x.name == dt.Rows[i]["Status Mat"].ToString().ToUpper()).FirstOrDefault();
+                        if (keyValue_statusMatIdByRow != null)
+                        {
+                            dt.Rows[i]["tB_Act_master_list_choice_id_InOutStock"] = keyValue_statusMatIdByRow.id;
+                        }
+                        else
+                        {
+                            validateValue = false;
+                            txtError += "รูปแบบข้อมูลที่กรอกไม่ถูกต้อง Status Mat บรรทัดที่ " + rowInExcelAlert + "<br />";
+                        }
+
+                    }
+                    if (validateValue == false)
+                    {
+                        ViewBag.Error = txtError;
+                        return View();
+                    }
+                    else
+                    {
+
+                    }
+                    //===END==validate=======
+
+                }
+
                 ViewBag.Result = "Successfully Imported";
             }
-            //Thread.Sleep(3000);
             return View();
         }
     }
