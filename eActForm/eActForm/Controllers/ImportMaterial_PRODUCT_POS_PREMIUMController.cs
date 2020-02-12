3a20d10ea942fa91a69dc16b30137ac22bf108ad
peerapop.int <peerapop.int@gmail.com>
2020-02-12 12:08:44 +0700

    bak20200212_1208

---
+++ eActForm/eActForm/Controllers/ImportMaterial_PRODUCT_POS_PREMIUMController.cs
@@ -0,0 +1,40 @@
+﻿using eActForm.BusinessLayer;
+using eActForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eActForm.Controllers
+{
+    [LoginExpire]
+    public class ImportMaterial_PRODUCT_POS_PREMIUMController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ImportExcel importExcel)
+        {
+            if (ModelState.IsValid)
+            {
+                string folderKeepFile = "ImportMatProductPosPremium";
+                string UploadDirectory = Server.MapPath("~") + "\\Uploadfiles\\" + folderKeepFile + "\\";
+                string path = UploadDirectory + importExcel.file.FileName;
+                importExcel.file.SaveAs(path);
+
+                var dt = new DataTable();
+                dt = ExcelAppCode.ReadExcel(path, "dataimport", "A:H");
+                var countDataHaveRows = dt.Rows.Count;
+
+                ViewBag.Result = "Successfully Imported";
+            }
+            //Thread.Sleep(3000);
+            return View();
+        }
+    }
+}
