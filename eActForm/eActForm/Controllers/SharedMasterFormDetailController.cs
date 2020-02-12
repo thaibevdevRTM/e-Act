5b5198b9e06bc60360843b6907d7af69a5c2aff1
tanapong <tanapong.w@thaibev.com>
2020-02-12 16:04:11 +0700

    add preview perry

--- eActForm/eActForm/Controllers/SharedMasterFormDetailController.cs
+++ eActForm/eActForm/Controllers/SharedMasterFormDetailController.cs
@@ -74,6 +74,10 @@ namespace eActForm.Controllers
         {
             return PartialView(activity_TBMMKT_Model);
         }
+        public ActionResult exPerryCashDetail(Activity_TBMMKT_Model activity_TBMMKT_Model)
+        {
+            return PartialView(activity_TBMMKT_Model);
+        }
 
     }
 }
