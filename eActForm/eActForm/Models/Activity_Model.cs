e92bbae81f65d734800bc77be2c2d2414b2b7bdb
peerapop.int <peerapop.int@gmail.com>
2020-03-12 17:07:43 +0700

    bak20200312_1707

--- eActForm/eActForm/Models/Activity_Model.cs
+++ eActForm/eActForm/Models/Activity_Model.cs
@@ -70,6 +70,8 @@ namespace eActForm.Models
             public string id { get; set; }
             public string statusId { get; set; }
             public string statusName { get; set; }
+            public string statusNameEN { get; set; }
+            public string languageDoc { get; set; }
             public string activityNo { get; set; }
             public DateTime? documentDate { get; set; }
             public string reference { get; set; }
