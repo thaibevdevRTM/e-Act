66b6a28db0c6215a3a0d452050442874561bb3e9
parnupongk <parnupongk@gmail.com>
2020-03-24 09:17:24 +0700

    update form order

---
+++ eActForm/eActForm/Controllers/frmOrderProduct/OrderProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eActForm.Controllers
+{
+    public class OrderProductController : Controller
+    {
+        // GET: OrderProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
