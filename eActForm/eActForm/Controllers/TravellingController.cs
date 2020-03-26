317271c3a54dd8d617c48b6dd770c8c2720675d7
kanok16 <39508872+kanok16@users.noreply.github.com>
2020-03-26 14:06:50 +0700

    commit

--- eActForm/eActForm/Controllers/TravellingController.cs
+++ eActForm/eActForm/Controllers/TravellingController.cs
@@ -28,21 +28,36 @@ namespace eActForm.Controllers
             }
             return PartialView(activity_TBMMKT_Model);
         }
+        public ActionResult travelling2y(Activity_TBMMKT_Model activity_TBMMKT_Model)
+        {
+            if (activity_TBMMKT_Model.placeDetailModel.Count == 0)
+            {
+                List<PlaceDetailModel> placeDetailModel = new List<PlaceDetailModel>();
+                for (int i = 0; i < 3; i++)
+                {
+                    placeDetailModel.Add(new PlaceDetailModel() { place = "", forProject = "", period = "", departureDate = null, arrivalDate = null });
+                }
+                activity_TBMMKT_Model.placeDetailModel = placeDetailModel;
+            }
+            return PartialView(activity_TBMMKT_Model);
+        }
+
+       
         public ActionResult expensesTrvDetail(Activity_TBMMKT_Model activity_TBMMKT_Model)
         {
-            if (activity_TBMMKT_Model.expensesDetailModel == null || activity_TBMMKT_Model.expensesDetailModel.costDetailLists == null || !activity_TBMMKT_Model.expensesDetailModel.costDetailLists.Any())
-            {
-                CostDetailOfGroupPriceTBMMKT model = new CostDetailOfGroupPriceTBMMKT
-                {
-                    costDetailLists = new List<CostThemeDetailOfGroupByPriceTBMMKT>()
-                };
-                for (int i = 0; i < 6; i++)
-                {
-                    model.costDetailLists.Add(new CostThemeDetailOfGroupByPriceTBMMKT() { productDetail = "", QtyName = "", unitPrice = 0, typeTheme = "", unit = 0, total = 0 });
-                }
-                activity_TBMMKT_Model.expensesDetailModel = model;
+            //if (activity_TBMMKT_Model.expensesDetailModel == null || activity_TBMMKT_Model.expensesDetailModel.costDetailLists == null || !activity_TBMMKT_Model.expensesDetailModel.costDetailLists.Any())
+            //{
+            //    CostDetailOfGroupPriceTBMMKT model = new CostDetailOfGroupPriceTBMMKT
+            //    {
+            //        costDetailLists = new List<CostThemeDetailOfGroupByPriceTBMMKT>()
+            //    };
+            //    for (int i = 0; i < 8; i++)
+            //    {
+            //        model.costDetailLists.Add(new CostThemeDetailOfGroupByPriceTBMMKT() { productDetail = "", QtyName = "", unitPrice = 0, typeTheme = "", unit = 0, total = 0 });
+            //    }
+            //    activity_TBMMKT_Model.expensesDetailModel = model;
 
-            }
+            //}
             return PartialView(activity_TBMMKT_Model);
         }
     }
