262da64f6866158689793ecc0e9131da20162e1b
parnupongk <parnupongk@gmail.com>
2020-03-27 13:01:16 +0700

    fixed case order on week

--- eActForm/eActForm/Controllers/frmOrderProduct/OrderProductController.cs
+++ eActForm/eActForm/Controllers/frmOrderProduct/OrderProductController.cs
@@ -8,6 +8,7 @@ using System.Configuration;
 using eActForm.BusinessLayer;
 using eForms.Presenter.MasterData;
 using eForms.Models.MasterData;
+using eForms.Presenter.Forms;
 
 namespace eActForm.Controllers
 {
@@ -15,6 +16,7 @@ namespace eActForm.Controllers
     {
         string strRemark = "พนักงาน {0} ชื่อ {1} เบอร์ติดต่อ {2} email {3} สั่งสินค้าส่งจังหวัด {4} หน่วย {5}";
         string strBodyDetail = "สินค้า {0} ราคาต่อหน่วย {1} จำนวน {2} ราคา {3} <br/>";
+        string strOrderDupInWeek = "สามารถสั่งได้สัปดาห์ละ 1 ครั้งค่ะ";
         // GET: OrderProduct
         public ActionResult index()
         {
@@ -30,50 +32,66 @@ namespace eActForm.Controllers
             var resultAjax = new AjaxResult();
             try
             {
-                string strProductDetail = "";
-                model.activityFormModel.documentDate = DateTime.Now;
-                model.activityFormModel.createdDate = DateTime.Now;
-                model.activityFormModel.updatedDate = DateTime.Now;
-                model.activityFormModel.updatedByUserId = model.activityFormModel.empId;
-                model.activityFormModel.createdByUserId = model.activityFormModel.empId;
-                model.activityFormModel.companyId = ConfigurationManager.AppSettings["companyId_Thaibev"];
-                model.activityFormModel.activityDetail = string.Format(strRemark, model.activityFormModel.empId
-                    , model.activityFormModel.customerName
-                    , model.activityFormModel.empTel
-                    , model.activityFormModel.empEmail
-                    , model.activityFormModel.regionName
-                    , model.activityFormModel.activityName);
+                DateTime baseDate = DateTime.Today;
+                var today = baseDate;
+                var yesterday = baseDate.AddDays(-1);
+                var thisWeekStart = baseDate.AddDays(-(int)baseDate.DayOfWeek).AddDays(+3);
+                var thisWeekEnd = thisWeekStart.AddDays(7).AddSeconds(-1);
 
-                if (ActivityFormCommandHandler.insertActivityForm(model.activityFormModel) > 0)
+                if (activityFormOrderProductPresenter.getCountOrderProductOnweek(AppCode.StrCon, model.activityFormModel.empId, thisWeekStart.ToString(), thisWeekEnd.ToString()) > 0)
                 {
-                    foreach (CostThemeDetail detail in model.costthemedetail)
-                    {
-                        detail.id = Guid.NewGuid().ToString();
-                        detail.activityId = model.activityFormModel.id;
-                        detail.createdDate = DateTime.Now;
-                        detail.updatedDate = DateTime.Now;
-                        detail.createdByUserId = model.activityFormModel.empId;
-                        detail.updatedByUserId = model.activityFormModel.empId;
-                        strProductDetail += detail.unit > 0 ? string.Format(strBodyDetail, detail.productDetail, detail.wholeSalesPrice, detail.unit, detail.total) : "";
-                        ActivityFormCommandHandler.insertEstimate(detail);
-                    }
+                    resultAjax.Code = 501;
+                    resultAjax.Message = strOrderDupInWeek;
                 }
+                else
+                {
+                    #region order product
+                    string strProductDetail = "";
+                    model.activityFormModel.documentDate = DateTime.Now;
+                    model.activityFormModel.createdDate = DateTime.Now;
+                    model.activityFormModel.updatedDate = DateTime.Now;
+                    model.activityFormModel.updatedByUserId = model.activityFormModel.empId;
+                    model.activityFormModel.createdByUserId = model.activityFormModel.empId;
+                    model.activityFormModel.companyId = ConfigurationManager.AppSettings["companyId_Thaibev"];
+                    model.activityFormModel.activityDetail = string.Format(strRemark, model.activityFormModel.empId
+                        , model.activityFormModel.customerName
+                        , model.activityFormModel.empTel
+                        , model.activityFormModel.empEmail
+                        , model.activityFormModel.regionName
+                        , model.activityFormModel.activityName);
 
-                List<SalesTeamCVMModel> contactModel = SalesTeamPresenter.getSalesTeamCVMBySaleTeamId(AppCode.StrConAuthen, model.activityFormModel.reference);
-                string strBody = string.Format(ConfigurationManager.AppSettings["bodyEmailConfirmOrderProduct"]
-                    , model.activityFormModel.customerName
-                    , strProductDetail
-                    , "จังหวัด " + model.activityFormModel.regionName + model.activityFormModel.activityName
-                    , contactModel[0].nameCashier1
-                    , contactModel[0].telCashier);
+                    if (ActivityFormCommandHandler.insertActivityForm(model.activityFormModel) > 0)
+                    {
+                        foreach (CostThemeDetail detail in model.costthemedetail)
+                        {
+                            detail.id = Guid.NewGuid().ToString();
+                            detail.activityId = model.activityFormModel.id;
+                            detail.createdDate = DateTime.Now;
+                            detail.updatedDate = DateTime.Now;
+                            detail.createdByUserId = model.activityFormModel.empId;
+                            detail.updatedByUserId = model.activityFormModel.empId;
+                            strProductDetail += detail.unit > 0 ? string.Format(strBodyDetail, detail.productDetail, detail.wholeSalesPrice, detail.unit, detail.total) : "";
+                            ActivityFormCommandHandler.insertEstimate(detail);
+                        }
+                    }
 
-                EmailAppCodes.sendEmail(model.activityFormModel.empEmail
-                    , ConfigurationManager.AppSettings["emailApproveCC"] // model.activityFormModel.contactEmail
-                    , ConfigurationManager.AppSettings["subjectEmailConfirm"]
-                    , strBody
-                    , null);
+                    List<SalesTeamCVMModel> contactModel = SalesTeamPresenter.getSalesTeamCVMBySaleTeamId(AppCode.StrConAuthen, model.activityFormModel.reference);
+                    string strBody = string.Format(ConfigurationManager.AppSettings["bodyEmailConfirmOrderProduct"]
+                        , model.activityFormModel.customerName
+                        , strProductDetail
+                        , "จังหวัด " + model.activityFormModel.regionName + " " + model.activityFormModel.activityName
+                        , contactModel[0].nameCashier1
+                        , contactModel[0].telCashier);
 
-                resultAjax.Code = 200;
+                    EmailAppCodes.sendEmail(model.activityFormModel.empEmail
+                        , ConfigurationManager.AppSettings["emailApproveCC"] // model.activityFormModel.contactEmail
+                        , ConfigurationManager.AppSettings["subjectEmailConfirm"]
+                        , strBody
+                        , null);
+
+                    resultAjax.Code = 200;
+                    #endregion
+                }
             }
             catch (Exception ex)
             {
