8244fc561a1aeec71734a3399407ce5664a0aa23
tanapong <tanapong.w@thaibev.com>
2020-03-28 22:41:59 +0700

    del menu

--- eActForm/eActForm/BusinessLayer/Appcodes/ApproveFlowAppCode.cs
+++ eActForm/eActForm/BusinessLayer/Appcodes/ApproveFlowAppCode.cs
@@ -131,7 +131,7 @@ namespace eActForm.BusinessLayer
                     {
                         if (ConfigurationManager.AppSettings["masterEmpExpense"] == getMasterType)
                         {
-                            model.flowDetail = getFlowDetailExpense(actFormId);
+                            model.flowDetail = getFlowDetailExpense(model.flowMain.id ,actFormId);
                         }
                         else
                         {
@@ -302,13 +302,13 @@ namespace eActForm.BusinessLayer
             }
         }
 
-        public static List<ApproveFlowModel.flowApproveDetail> getFlowDetailExpense(string actId)
+        public static List<ApproveFlowModel.flowApproveDetail> getFlowDetailExpense(string flowId ,string actId)
         {
             try
             {
                 DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure, "usp_getFlowApproveDetailExpense"
-                    , new SqlParameter[] {new SqlParameter("@actFormId",actId)
-                    });
+                    , new SqlParameter[]{ new SqlParameter("@flowId", flowId)
+                                   , new SqlParameter("@actFormId", actId)});
                 var lists = (from DataRow dr in ds.Tables[0].Rows
                              select new ApproveFlowModel.flowApproveDetail()
                              {
