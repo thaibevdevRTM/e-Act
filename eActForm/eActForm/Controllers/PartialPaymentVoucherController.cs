7384b4229c86e31f2cc440e146635c123c7b5e26
peerapop.int <peerapop.int@gmail.com>
2020-04-01 14:42:30 +0700

    beforeItext7

--- eActForm/eActForm/Controllers/PartialPaymentVoucherController.cs
+++ eActForm/eActForm/Controllers/PartialPaymentVoucherController.cs
@@ -8,9 +8,13 @@ namespace eActForm.Controllers
     {
         public ActionResult headerPv(Activity_TBMMKT_Model activity_TBMMKT_Model)
         {
-            return PartialView(activity_TBMMKT_Model);
+            return PartialView(activity_TBMMKT_Model); 
         }
         public ActionResult headerPvDetails(Activity_TBMMKT_Model activity_TBMMKT_Model)
+        {
+            return PartialView(activity_TBMMKT_Model);
+        }
+        public ActionResult detailSectionOneToFive(Activity_TBMMKT_Model activity_TBMMKT_Model)
         {
             return PartialView(activity_TBMMKT_Model);
         }
