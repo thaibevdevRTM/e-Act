db6a5f79ed5ab4fe9b237f3e0903103a4e8474dc
kanok16 <39508872+kanok16@users.noreply.github.com>
2020-04-23 16:34:50 +0700

    commit

---
+++ eActForm/eActForm/BusinessLayer/QueryHandler/QueryGetPosition.cs
@@ -0,0 +1,36 @@
+﻿using eActForm.Models;
+using Microsoft.ApplicationBlocks.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using WebLibrary;
+
+namespace eActForm.BusinessLayer.QueryHandler
+{
+    public class QueryGetPosition
+    {
+        public static List<positionModel> getNewlinePosition()
+        {
+            try
+            {              
+                   DataSet ds = SqlHelper.ExecuteDataset(AppCode.StrCon, CommandType.StoredProcedure,
+                    "usp_getNewlinePosition");
+                
+                var lists = (from DataRow d in ds.Tables[0].Rows
+                             select new positionModel()
+                             {
+                                 newPositionName = d["newPositionName"].ToString(),
+                                 positionName = d["positionName"].ToString(),
+                             });
+                return lists.ToList();      
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.WriteError("getAllSubject => " + ex.Message);
+                return new List<positionModel>();
+            }
+        }
+    }
+}
