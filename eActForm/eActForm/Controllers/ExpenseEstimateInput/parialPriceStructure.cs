a227b0c28f42abbd2b482e6bcb78b30f9b74b2db
Tanapong Wanichayatakul <70008316@THAIBEV.COM>
2020-05-19 08:54:32 +0700

    commit

---
+++ eActForm/eActForm/Controllers/ExpenseEstimateInput/parialPriceStructure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eActForm.Controllers
+{
+    public partial class ExpenseEstimateInputController
+    {
+        // GET: parialPriceStructureList
+        public ActionResult parialPriceStructure()
+        {
+            return View();
+        }
+    }
+}
