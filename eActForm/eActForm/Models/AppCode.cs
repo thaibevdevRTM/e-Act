cab2eb7febc44f379727ec8b169e500eed2325f1
kanok16 <39508872+kanok16@users.noreply.github.com>
2020-07-29 08:55:57 +0700

    commit

--- eActForm/eActForm/Models/AppCode.cs
+++ eActForm/eActForm/Models/AppCode.cs
@@ -91,6 +91,7 @@ namespace eActForm.Models
         {
             public const string Allowance = "06FF853F-EBB0-48E8-9620-520D0B8F6E0C";
             public const string Medical = "6BB0F68F-4B07-4E00-9B1E-B776D003D992";
+            public const string hotelExpense = "3FB9A4DC-9CE9-49D9-A68D-AAE0455BB6D1";
         }
 
 
