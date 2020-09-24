b86df033087eec695ba00397e437a1e600fe33e6
tanapong.w@thaibev.com <tanapong.w@thaibev.com>
2020-09-24 15:54:03 +0700

    add order by estimate ใบเบิกค่าใช้จ่าย

--- eActForm/eActForm/Controllers/SharedMasterFormDetail_InputController.cs
+++ eActForm/eActForm/Controllers/SharedMasterFormDetail_InputController.cs
@@ -156,14 +156,14 @@ namespace eActForm.Controllers
             try
             {
                 var estimateList = QueryGetActivityEstimateByActivityId.getByActivityId(activity_TBMMKT_Model.activityFormModel.id);
-                activity_TBMMKT_Model.activityOfEstimateList = estimateList.Where(x => x.activityTypeId.Equals("1")).ToList();
+                activity_TBMMKT_Model.activityOfEstimateList = estimateList.Where(x => x.activityTypeId.Equals("1")).OrderBy(x => x.rowNo).ToList();
                 if (!activity_TBMMKT_Model.activityOfEstimateList.Any())
                 {
                     activity_TBMMKT_Model.activityOfEstimateList.Add(new CostThemeDetailOfGroupByPriceTBMMKT());
                     activity_TBMMKT_Model.activityFormModel.mode = AppCode.Mode.addNew.ToString();
                 }
 
-                activity_TBMMKT_Model.activityOfEstimateList2 = estimateList.Where(x => x.activityTypeId.Equals("2")).ToList();
+                activity_TBMMKT_Model.activityOfEstimateList2 = estimateList.Where(x => x.activityTypeId.Equals("2")).OrderBy(x => x.rowNo).ToList();
                 if (!activity_TBMMKT_Model.activityOfEstimateList2.Any())
                 {
                     activity_TBMMKT_Model.activityOfEstimateList2.Add(new CostThemeDetailOfGroupByPriceTBMMKT());
