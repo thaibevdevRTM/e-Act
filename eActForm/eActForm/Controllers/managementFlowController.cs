1bb8319acc09a07816b279d782f3d5d935e8f026
tanapong.w@thaibev.com <tanapong.w@thaibev.com>
2020-10-29 12:41:50 +0700

    commit

--- eActForm/eActForm/Controllers/managementFlowController.cs
+++ eActForm/eActForm/Controllers/managementFlowController.cs
@@ -69,6 +69,7 @@ namespace eActForm.Controllers
             management_Model.p_channelId = model.channelId;
             management_Model.p_subjectId = model.subjectId;
             management_Model.activityTypeId = model.activityGroup;
+            management_Model.p_companyId = model.companyId;
 
             TempData["management_Model"] = management_Model;
             return RedirectToAction("approveList");
