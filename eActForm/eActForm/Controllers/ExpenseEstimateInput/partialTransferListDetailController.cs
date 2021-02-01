5c05171deaf83734761536a24a4aaebcc098fcda
tanapong.w@thaibev.com <tanapong.w@thaibev.com>
2021-02-01 10:04:44 +0700

    transfer bg

---
+++ eActForm/eActForm/Controllers/ExpenseEstimateInput/partialTransferListDetailController.cs
@@ -0,0 +1,14 @@
+﻿using eActForm.Models;
+using System.Web.Mvc;
+
+namespace eActForm.Controllers
+{
+    public partial class ExpenseEstimateInputController
+    {
+        // GET: partialTransferListDetail
+        public ActionResult partialTransferList(Activity_TBMMKT_Model activity_TBMMKT_Model)
+        {
+            return PartialView(activity_TBMMKT_Model);
+        }
+    }
+}
