fb2f41e36b07139be45032440c53e19d5f178dc2
comeonbebe <tanapong.w@thaibev.com>
2021-11-15 13:31:23 +0700

    commit

--- eActForm/eForms.Presenter/AppCode/pManagementFlowAppCode.cs
+++ eActForm/eForms.Presenter/AppCode/pManagementFlowAppCode.cs
@@ -17,7 +17,7 @@ namespace eForms.Presenter.AppCode
         {
             try
             {
-                DataSet ds = SqlHelper.ExecuteDataset(strCon, CommandType.StoredProcedure, "usp_getFlowApproveByEmpId"
+                DataSet ds = SqlHelper.ExecuteDataset(strCon, CommandType.StoredProcedure, "usp_getFlowbyEmpId"
                     , new SqlParameter("@empId", empId));
                 var lists = (from DataRow d in ds.Tables[0].Rows
                              select new ManagentFlowModel.flowSubject()
@@ -64,6 +64,52 @@ namespace eForms.Presenter.AppCode
             catch (Exception ex)
             {
                 throw new Exception("updateSwapByApproveId >>" + ex.Message);
+            }
+        }
+
+        public static List<ManagentFlowModel.flowSubject> getFlowbyEmpId(string strCon,  string empId)
+        {
+            try
+            {
+                DataSet ds = SqlHelper.ExecuteDataset(strCon, CommandType.StoredProcedure, "usp_getFlowbyEmpId"
+                    , new SqlParameter("@empId", empId));
+                var lists = (from DataRow d in ds.Tables[0].Rows
+                             select new ManagentFlowModel.flowSubject()
+                             {
+                                 flowId = d["flowId"].ToString(),
+                                 subjectName = d["subjectName"].ToString(),
+                                 companyName = d["companyNameTH"].ToString(),
+                                 limitName = d["limitName"].ToString(),
+
+                             });
+                return lists.ToList();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.WriteError("getFlowbyEmpId => " + ex.Message);
+                return new List<ManagentFlowModel.flowSubject>();
+            }
+        }
+
+        public static int updateCompanyFlowByEmpId(string strCon,string flowId,string companyId, string empId,string updatedByUserId)
+        {
+            try
+            {
+                int result = 0;
+                result = SqlHelper.ExecuteNonQuery(strCon, CommandType.StoredProcedure, "usp_updateFlowCompanyByEmpId"
+                     , new SqlParameter[] {new SqlParameter("@flowId",flowId)
+                     ,new SqlParameter("@companyId",companyId)
+                     ,new SqlParameter("@empId",empId)
+                     ,new SqlParameter("@updatedByUserId",updatedByUserId)
+                     });
+
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("updateCompanyFlowByEmpId >>" + ex.Message);
+
             }
         }
     }
