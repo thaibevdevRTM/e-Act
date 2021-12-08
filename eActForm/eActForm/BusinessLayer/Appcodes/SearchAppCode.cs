33ce08611fd8180b3546dd2f6ee893ae02174390
comeonbebe <tanapong.w@thaibev.com>
2021-12-08 14:27:19 +0700

    commit

--- eActForm/eActForm/BusinessLayer/Appcodes/SearchAppCode.cs
+++ eActForm/eActForm/BusinessLayer/Appcodes/SearchAppCode.cs
@@ -25,7 +25,6 @@ namespace eActForm.BusinessLayer
                     masterTypeFormList = QueryGet_master_type_form.get_master_type_form("").Where(x => x.companyId == UtilsAppCode.Session.User.empCompanyId).ToList(),
                     departmentList = QueryOtherMaster.getOhterMaster("department", "search").ToList(),
                     channelList = QueryGetAllChanel.getAllChanel().Where(x => x.typeChannel == "data").ToList(),
-                    brandList = QueryGetAllBrand.GetAllBrand(),
                     activityGroupList = QueryGetAllActivityGroup.getAllActivityGroup().Where(x => x.activityCondition.Contains(typeForm))
                    .GroupBy(item => item.activitySales)
                    .Select(grp => new TB_Act_ActivityGroup_Model { id = grp.First().id, activitySales = grp.First().activitySales }).ToList()
@@ -52,6 +51,9 @@ namespace eActForm.BusinessLayer
                     QueryGetAllCustomers.getCustomersOMT().Where(x => x.cusNameEN != "").ToList(),
                     productTypelist = QuerygetAllProductCate.getProductTypeByEmpId(),
                     productBrandList = QueryGetAllBrand.GetAllBrand().OrderBy(x => x.brandName).ToList(),
+                    masterTypeFormList = QueryGet_master_type_form.get_master_type_form("").Where(x => x.companyId == UtilsAppCode.Session.User.empCompanyId).ToList(),
+                    departmentList = QueryOtherMaster.getOhterMaster("department", "search").ToList(),
+                    channelList = QueryGetAllChanel.getAllChanel().Where(x => x.typeChannel == "data").ToList(),
                     //activityGroupList = QueryGetAllActivityGroup.getAllActivityGroup().Where(x => x.activityCondition.Contains(typeForm))
                     activityGroupList = QueryGetAllActivityGroup.getAllActivityGroup().Where(x => x.activityCondition.Contains(typeForm) & (x.activitySales == "Promotion Support" || x.activitySales == "Special Discount"))
                    .GroupBy(item => item.activitySales)
@@ -75,6 +77,10 @@ namespace eActForm.BusinessLayer
                     productGroupList = QueryGetAllProductGroup.getAllProductGroup(),
                     customerslist = @UtilsAppCode.Session.User.empCompanyId == ConfigurationManager.AppSettings["companyId_MT"] ? QueryGetAllCustomers.getCustomersMT().Where(x => x.cusNameEN != "").ToList() : QueryGetAllCustomers.getCustomersOMT().Where(x => x.cusNameEN != "").ToList(),
                     productTypelist = QuerygetAllProductCate.getAllProductType(),
+                    masterTypeFormList = QueryGet_master_type_form.get_master_type_form("").Where(x => x.companyId == UtilsAppCode.Session.User.empCompanyId).ToList(),
+                    productBrandList = QueryGetAllBrand.GetAllBrand().OrderBy(x => x.brandName).ToList(),
+                    departmentList = QueryOtherMaster.getOhterMaster("department", "search").ToList(),
+                    channelList = QueryGetAllChanel.getAllChanel().Where(x => x.typeChannel == "data").ToList(),
                     activityGroupList = QueryGetAllActivityGroup.getAllActivityGroup()
                     .GroupBy(item => item.activitySales)
                     .Select(grp => new TB_Act_ActivityGroup_Model { id = grp.First().id, activitySales = grp.First().activitySales }).ToList()
