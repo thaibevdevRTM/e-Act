d5ee97ceaa2088efd97b3fb3bebf7ea995b0e60d
comeonbebe <48464417+comeonbebe@users.noreply.github.com>
2023-01-11 15:12:08 +0700

    create new ui core

---
+++ eActForm/eForms.UI/Controllers/ActivityHistoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eForms.UI.Controllers
+{
+    public class ActivityHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
