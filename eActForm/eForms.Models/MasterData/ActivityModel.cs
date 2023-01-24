a268e0ba2b10ad12e958ce6145aa03f14ade90b7
comeonbebe <48464417+comeonbebe@users.noreply.github.com>
2023-01-24 15:51:55 +0700

    add project UI

---
+++ eActForm/eForms.Models/MasterData/ActivityModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eForms.Models.MasterData
+{
+    public class ActivityModel
+    {
+        public ActivityModels activityModels { get; set; }
+        public List<TB_Act_ProductBrand_Model> brandList { get; set; }
+        public List<TB_Act_Chanel_Model> chanelList { get; set; }
+        public List<TB_Act_ActivityGroup_Model> activityGroupList { get; set; }
+
+    }
+
+
+    public class ActivityModels
+    {
+        public string brandId { get; set; }
+        public string channelId { get; set; }
+        public string activityGroupId { get; set; }
+        public string actType { get; set; }
+        public string statusId { get; set; }
+        public string activityNo { get; set; }
+        public DateTime documentDate { get; set; }
+        public DateTime activityPeriodSt { get; set; }
+        public DateTime activityPeriodEnd { get; set; }
+        public string activityName { get; set; }
+        public string objective { get; set; }
+        public DateTime createdDate { get; set; }
+        public DateTime createdByUserId { get; set; }
+        public DateTime updatedDate { get; set; }
+    }
+
+}
