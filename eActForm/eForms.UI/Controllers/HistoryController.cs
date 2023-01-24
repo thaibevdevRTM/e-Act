7174cf1aca16f410095fbce99a274f66d3ae06f3
comeonbebe <48464417+comeonbebe@users.noreply.github.com>
2023-01-24 16:19:52 +0700

    commit UI

---
+++ eActForm/eForms.UI/Controllers/HistoryController.cs
@@ -0,0 +1,23 @@
+﻿using eForms.Models.MasterData;
+using eForms.UI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eForms.UI.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IConfiguration _config = null;
+        public HistoryController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            ActivityModel activityModel = new ActivityModel();
+            activityModel.brandList = HistoryHelpercs.GetBrandBudgetControl();
+
+            return View();
+        }
+    }
+}
---
+++ eActForm/eForms.UI/Helpers/HistoryHelpercs.cs
@@ -0,0 +1,36 @@
+﻿using eForms.Models.MasterData;
+using eForms.UI.Controllers;
+using RepoDb;
+using System.Data.SqlClient;
+
+namespace eForms.UI.Helpers
+{
+    public class HistoryHelpercs
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static IConfiguration _config = null;
+        public HistoryHelpercs(IConfiguration config)
+        {
+            _config = config;
+        }
+        public static List<TB_Act_ProductBrand_Model> GetBrandBudgetControl()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_config["AppSettings:DBConnection"].ToString()))
+                {
+
+                    var extractor = connection.ExecuteQueryMultiple("EXEC [dbo].[usp_getProductBrandBGControl];");
+                     return extractor.Extract<TB_Act_ProductBrand_Model>().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return new List<TB_Act_ProductBrand_Model>();
+            }
+
+        }
+
+        
+    }
+}
