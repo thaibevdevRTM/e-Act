84f067df11d74c8683c1bf62ef923d49d7149984
comeonbebe <48464417+comeonbebe@users.noreply.github.com>
2023-01-24 15:49:54 +0700

    clear code

--- eActForm/eForms.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace eForms.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
