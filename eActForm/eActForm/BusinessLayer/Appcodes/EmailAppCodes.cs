ad2ecc2bfca9ef9d986730a9f265de4ca1e47c1c
Tanapong Wanichayatakul <70008316@THAIBEV.COM>
2023-09-26 10:38:33 +0700

    check approve

--- eActForm/eActForm/BusinessLayer/Appcodes/EmailAppCodes.cs
+++ eActForm/eActForm/BusinessLayer/Appcodes/EmailAppCodes.cs
@@ -660,7 +660,7 @@ namespace eActForm.BusinessLayer
                 activity_TBMMKT_Model = ActivityFormTBMMKTCommandHandler.getDataForEditActivity(actId);
                 var emailTypeTxt = "";
                 string[] arrayFormStyleV1 = { ConfigurationManager.AppSettings["formBgTbmId"], ConfigurationManager.AppSettings["formAdvTbmId"], ConfigurationManager.AppSettings["formAdvHcmId"], ConfigurationManager.AppSettings["masterEmpExpense"], ConfigurationManager.AppSettings["formPaymentVoucherTbmId"], ConfigurationManager.AppSettings["formReceptions"], ConfigurationManager.AppSettings["formPurchaseTbm"] };
-                string[] arrayFormStyleV2 = { ConfigurationManager.AppSettings["formPosTbmId"], ConfigurationManager.AppSettings["formTrvTbmId"], ConfigurationManager.AppSettings["formTrvHcmId"], ConfigurationManager.AppSettings["formExpTrvNumId"], ConfigurationManager.AppSettings["formExpMedNumId"], ConfigurationManager.AppSettings["formCR_IT_FRM_314"] };
+                string[] arrayFormStyleV2 = { ConfigurationManager.AppSettings["formPosTbmId"], ConfigurationManager.AppSettings["formTrvTbmId"], ConfigurationManager.AppSettings["formTrvHcmId"], ConfigurationManager.AppSettings["formExpTrvNumId"], ConfigurationManager.AppSettings["formExpMedNumId"], ConfigurationManager.AppSettings["formCR_IT_FRM_314"], ConfigurationManager.AppSettings["formTrvHome"] };
                 string[] arrayFormStyleV3 = { ConfigurationManager.AppSettings["formPosTbmId"], ConfigurationManager.AppSettings["formCR_IT_FRM_314"] };
                 string[] arrayFormStyleV4 = { ConfigurationManager.AppSettings["formCR_IT_FRM_314"] };
 
